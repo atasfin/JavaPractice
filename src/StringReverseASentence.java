75a02e987b1b1a1b57aeefc75c62b5c3256d592c
Tasfin <sheikh.tasfin@globaltranz.com>
2022-10-26 18:36:18 -0500

    Added no 10

--- src/StringReverseASentence.java
+++ src/StringReverseASentence.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StringReverseASentence {
@@ -11,7 +12,24 @@ public class StringReverseASentence {
 
     public static void reverseSentence(String str){
 
+        String stringArray[] = str.split(" ");
         List<String> reverseStringList = new ArrayList<String>();
+        String reverseSentence = "";
+
+        for(int i = stringArray.length-1; i>=0; i--){
+            reverseStringList.add(stringArray[i]);
+        }
+        System.out.println(reverseStringList);
+
+        for (String item : reverseStringList) {
+            reverseSentence = reverseSentence + item + " ";
+        }
+
+        System.out.println(reverseSentence);
+
+
+
+
 
     }
 
